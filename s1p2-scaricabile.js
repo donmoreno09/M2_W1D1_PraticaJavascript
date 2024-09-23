b6caa429c2d3febcc7bddbb7b816be29a66fde8c
donmoreno09 <morenodaqm@gmail.com>
2024-09-23 19:02:57 +0200

    Initial Commit

---
+++ s1p2-scaricabile.js
@@ -0,0 +1,76 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* In JavaScript ci sono diversi tipi di dati (datatypes):
+
+String: È come un insieme di lettere, parole o frasi. Per esempio, il tuo nome o un messaggio. Si scrive tra virgolette, tipo "Ciao!".
+
+Number: Sono tutti i numeri, sia interi come 7, che decimali come 3.14. Con i numeri possiamo fare addizioni, sottrazioni e così via.
+
+Boolean: È un dato che può essere solo vero o falso. Per esempio, true o false, usato per fare decisioni nel codice.
+
+Undefined: Questo tipo di dato appare quando una variabile non ha ancora un valore assegnato. Credo sia come una scatola vuota.
+
+Null: È come dire che una variabile ha proprio "nulla" al suo interno. Una scatola che esiste, ma che non contiene nulla o che contiene nessun valore.
+
+Object: È una specie di scatola che può contenere tanti valori di tipi diversi, organizzati come coppie chiave-valore. Per esempio, una persona può avere un nome, un’età, l'indirizzo e così via.
+
+Array: È una lista di valori. Può essere una lista di nomi, numeri, o qualsiasi altra cosa. Si scrive tra parentesi quadre, tipo [1, 2, 3]. */
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* Un oggetto in JavaScript è come una scatola che contiene delle proprietà, e ogni proprietà ha un nome e un valore. Ad esempio, pensa a una persona: una persona può avere un nome, un’età, un lavoro, ecc. */
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/*  let somma = 12 + 20;
+    console.log(somma);
+ */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* let x = 12; */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* let name = "Don"; */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/*  let risultato = 4 - x;
+    console.log(risultato);
+*/
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/*  let name1 = "john";
+    let name2 = "John";
+
+    console.log(name1 !== name2);
+    console.log(name1=== name2);
+*/
